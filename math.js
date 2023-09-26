1a4601b40f15f3c4c91d66ae5a72e4f17858c354
“arunagiri-3300” <arunagiri.92.ac@gmail.com>
2023-09-26 13:04:20 +0530

    js and style added

---
+++ math.js
@@ -0,0 +1,24 @@
+// This is a JavaScript file template
+
+// Declare variables and constants
+const pi = 3.14159;
+let counter = 0;
+
+// Define functions
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+// Main program
+console.log("Starting the program...");
+
+greet("John");
+
+const result = add(5, 7);
+console.log(`The result of addition is: ${result}`);
+
+console.log("Ending the program...");
